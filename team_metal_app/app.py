5ed96b0b844b33a944d16d741f23182dbd5d6a71
Piotr Kazmierczak <piotr.kazmierczak@mollie.com>
2021-10-13 14:30:17 +0200

    base model

--- team_metal_app/app.py
+++ team_metal_app/app.py
@@ -1,6 +1,8 @@
 import pickle
+from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,3 +14,15 @@ infile.close()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+@app.post("/predict")
+async def predict():
+    return {"message": "Go away"}
